fa2c763e1563089f302097b3a47fe113510f40b2
danyel <dpadayao40@gmail.com>
2025-05-11 09:11:31 +0800

    refactor: utilize api base genkit

--- src/app/(personal)/(home)/_actions/genkit.ts
+++ src/app/(personal)/(home)/_actions/genkit.ts
@@ -1,76 +1,21 @@
 'use server'
-import { genkit } from 'genkit'
-import { googleAI, gemini20Flash } from '@genkit-ai/googleai'
-const ai = genkit({
-  plugins: [
-    googleAI({
-      apiKey: process.env.GOOGLE_GENAI_API_KEY!,
-      apiVersion: ['v1beta']
-    })
-  ],
-  model: gemini20Flash, // set default model
-  promptDir: './prompts'
-})
-
-const askFlow = ai.prompt('danyel')
-
-// Start the conversation with a greeting
-
-function extractJSONFromResponse (response: string) {
-  try {
-    // Look for content between curly braces
-    const match = response.match(/{[\s\S]*}/)
-    if (match) {
-      return JSON.parse(match[0])
-    }
-    return response
-  } catch (error) {
-    console.error('Error extracting JSON:', error)
-    return null
-  }
-}
-
+import axios from 'axios'
 export async function chat (userInput: string, history: string[] = []) {
   try {
-    const result = {
-      text: '',
-      action: '',
-      history: [],
-      sender: ''
+    const response = await axios.post(`${process.env.API_URL}/chat`, {
+      text: userInput,
+      history
+    })
+
+    if (response.status !== 200) {
+      throw response.data
     }
-    if (userInput && typeof userInput === 'string') {
-      const { text: response, output } = await askFlow({
-        text: userInput,
-        history
-      })
-
-      const res = extractJSONFromResponse(response)
-
-      if (!!output) {
-        if (output.text !== undefined) {
-          result.text = output.text
-          result.action = output?.action || ''
-          result.history = output?.history || []
-          result.sender = output?.sender || 'ai'
-        } else {
-          result.text = res?.text || res || ''
-          result.action = res?.action || ''
-          result.history = res?.history || []
-          result.sender = res?.sender || 'ai'
-        }
-      } else {
-        result.text = res?.text || res || ''
-        result.action = res?.action || ''
-        result.history = res?.history || []
-        result.sender = res?.sender || 'ai'
-      }
-
-      return result
-    }
+    const res = response.data
+    return res
   } catch (error) {
     console.error(error)
     return {
-      text: 'Something went wrong. Please try again in a few minutes',
+      text: 'Something went wrong. Please try again in a few seconds.',
       action: '',
       history: [],
       sender: 'ai'
