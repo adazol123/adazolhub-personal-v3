0b97a9bb91e08b132e5bb9fbeb16f075c5173933
danyel <dpadayao40@gmail.com>
2025-05-14 17:25:52 +0800

    feat: implement expertise & experience section component, attach props value to summary section component

--- src/app/(personal)/availability/page.tsx
+++ src/app/(personal)/availability/page.tsx
@@ -1,13 +1,46 @@
 import React from 'react'
 import HeadingSection from './_components/heading'
 import SummarySection from './_components/summary'
+import ExpertiseSection from './_components/expertise.section'
+import { availabilityPayload, infoPayload } from '@/_temp/information.temp'
+import {
+  IconBrandAws,
+  IconBrandTypescript,
+  IconCodeDots,
+  IconMoodCheck,
+  IconServerBolt,
+  IconUsersGroup
+} from '@tabler/icons-react'
+import ExperienceSection from './_components/experience.section'
+
+const IconsExpertisesMapper = [
+  IconCodeDots,
+  IconBrandTypescript,
+  IconServerBolt,
+  IconBrandAws,
+  IconMoodCheck,
+  IconUsersGroup
+]
 
 const ResumePage = () => {
   return (
     <>
       <HeadingSection />
-      <SummarySection />
-      <section className='container max-w-prose mx-auto relative'></section>
+      <SummarySection
+        label={infoPayload.summary_label}
+        summary={infoPayload.summary}
+      />
+      <ExpertiseSection
+        label={availabilityPayload.expertise_label}
+        expertises={availabilityPayload.expertise.map((expertise, index) => ({
+          ...expertise,
+          icons: IconsExpertisesMapper[index]
+        }))}
+      />
+      <ExperienceSection
+        label={availabilityPayload.experience_label}
+        experiences={availabilityPayload.experience}
+      />
     </>
   )
 }
