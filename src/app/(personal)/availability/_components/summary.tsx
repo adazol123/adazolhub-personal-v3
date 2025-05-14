f3f51423088b059dc62d85924652bb5b037b1c86
danyel <dpadayao40@gmail.com>
2025-05-14 17:23:46 +0800

    refactor: implement props on summary section & add collapse toggler

--- src/app/(personal)/availability/_components/summary.tsx
+++ src/app/(personal)/availability/_components/summary.tsx
@@ -1,13 +1,33 @@
-import { infoPayload } from '@/_temp/information.temp'
+'use client'
+import { cn } from '@/lib/utils'
 import React from 'react'
 
-const SummarySection = () => {
+type SummarySectionProps = {
+  label: string
+  summary: string
+}
+
+const SummarySection = ({ label, summary }: SummarySectionProps) => {
+  const [seeMore, setSeeMore] = React.useState(false)
+  const toggleReadMore = () => {
+    setSeeMore(state => !state)
+  }
   return (
-    <div className='container max-w-prose mx-auto'>
-      <h3 className='text-xs text-neutral-400 mb-1'>
-        {infoPayload.summary_label}
+    <div className='container max-w-prose mx-auto my-4 bg-white p-4 rounded-2xl transition-all'>
+      <h3 className='text-forest-green-800 mb-2 font-medium text-sm'>
+        {label}
       </h3>
-      <p>{infoPayload.summary}</p>
+      <p className='leading-relaxed text-neutral-500'>
+        <span className={cn(seeMore ? 'line-clamp-none' : 'line-clamp-3')}>
+          {summary}
+        </span>
+        <span
+          className='underline text-accent underline-offset-2 cursor-pointer'
+          onClick={toggleReadMore}
+        >
+          {seeMore ? 'Read less' : 'Read more'}
+        </span>
+      </p>
     </div>
   )
 }
